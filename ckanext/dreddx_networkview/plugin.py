1d9e92269ff784b942e99096caad1891a6cda54e
tpgmartin <tpgmartin@gmail.com>
2022-08-13 19:13:19 +0100

    Add info method

--- ckanext/dreddx_networkview/plugin.py
+++ ckanext/dreddx_networkview/plugin.py
@@ -15,5 +15,15 @@ class NetworkView(plugins.SingletonPlugin):
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'dreddx_networkview')
 
+    def info(self):
+        return {'name': 'networkview',
+                'title': 'Network',
+                'icon': 'connectdevelop',
+                'iframed': False,
+                'filterable': False,
+                'schema': {},
+                }
+
+
     def view_template(self):
         return 'networkview_base.html'
